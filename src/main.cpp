0d435922133b8a4969ffc667576633aeff77d0e2
InvalidSE <taine.reader@gmail.com>
2023-04-15 10:54:37 +1200

    real?

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,90 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-// Bluetooth TX -> Arduino D8
-// Bluetooth RX -> Arduino D7
+// Bluetooth control pins
 const int BLUETOOTH_TX = 8;
 const int BLUETOOTH_RX = 7;
 
+// Motor control pin  s
+#define PWMA 5
+#define AIN1 3
+#define AIN2 4
+#define PWMB 6
+#define BIN1 10
+#define BIN2 9
+
+// Setup bluetooth serial
 SoftwareSerial bluetooth(BLUETOOTH_TX, BLUETOOTH_RX);
+char incomingData[20];
+int bearing, amplitude;
+
 
 void setup() {
-  // Begin serial communication at 9600 bps
   Serial.begin(9600);
-
-  // Begin serial communication with HM 10 at 9600 bps 
   bluetooth.begin(9600);
-
-  // delay in case the HM 10 needs time to respond
-  delay(100);
-  
-  Serial.println("Setup Complete");
+  pinMode(PWMA, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
 }
 
-
 void loop() {
-  if(bluetooth.available()) {
-    Serial.write(bluetooth.read());
+  // Bluetooth data comes in ')360,100)360,100' format where the first number is the bearing and the second is the amplitude.
+  if(bluetooth.available() > 0) {
+    while (bluetooth.available() > 0) {
+      // read max 10 bytes
+      bluetooth.readBytesUntil(')', incomingData, 10);
+    } 
+    Serial.println(incomingData);
+
+    sscanf(incomingData, "%d,%d", &bearing, &amplitude);
+    Serial.print("Bearing: ");
+    Serial.println(bearing);
+    Serial.print("Amplitude: ");
+    Serial.println(amplitude);
+
+    // Reset incoming data
+    memset(incomingData, 0, sizeof(incomingData));
   }
-  if(Serial.available()) {
-    bluetooth.write(Serial.read());
+
+  // Move motors
+  if (amplitude > 0) {
+    // 0 degrees is at the right of the joystick
+
+    if(bearing >= 0 && bearing <= 90) {
+      // Move forward
+      analogWrite(PWMA, amplitude);
+      digitalWrite(AIN1, HIGH);
+      digitalWrite(AIN2, LOW);
+      analogWrite(PWMB, amplitude);
+      digitalWrite(BIN1, HIGH);
+      digitalWrite(BIN2, LOW);
+    } else if(bearing > 90 && bearing <= 180) {
+      // Move left
+      analogWrite(PWMA, amplitude);
+      digitalWrite(AIN1, LOW);
+      digitalWrite(AIN2, HIGH);
+      analogWrite(PWMB, amplitude);
+      digitalWrite(BIN1, HIGH);
+      digitalWrite(BIN2, LOW);
+    } else if(bearing > 180 && bearing <= 270) {
+      // Move backward
+      analogWrite(PWMA, amplitude);
+      digitalWrite(AIN1, LOW);
+      digitalWrite(AIN2, HIGH);
+      analogWrite(PWMB, amplitude);
+      digitalWrite(BIN1, LOW);
+      digitalWrite(BIN2, HIGH);
+    } else if(bearing > 270 && bearing <= 360) {
+      // Move right
+      analogWrite(PWMA, amplitude);
+      digitalWrite(AIN1, HIGH);
+      digitalWrite(AIN2, LOW);
+      analogWrite(PWMB, amplitude);
+      digitalWrite(BIN1, LOW);
+      digitalWrite(BIN2, HIGH);
+    }
   }
 }
